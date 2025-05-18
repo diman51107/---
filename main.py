bd6be55ed18afe21fed89987be8a2ee511bc4763
diman51107 <patronishe42@mail.ru>
2025-05-18 16:52:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from pygame import *
 
-game = True
-pygame.init()
-# класс-родитель для других спрайтов
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
 
+window = display.set_mode((win_height, win_width))
+window.fill(back)
+
+'''необходимые классы'''
+# класс-родитель для других спрайтов
 class GameSprite(sprite.Sprite):
  # конструктор класса
     def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -24,12 +29,33 @@ class GameSprite(sprite.Sprite):
 # класс главного игрока
 class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_UP] and self.rect.y > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+'''добавление цикла'''
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+racket1 = Player('racket1.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket1.png', 520, 200, 4, 50, 150)
+ball = GameSprite('images.png', 200, 200, 4, 50, 50)
+speed_x = 3
+speed_y = 3
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 while game:
     for e in event.get():
         if e.type == QUIT:
